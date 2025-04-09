6bac1d2bc9a582054bcdf1b30d1703243f5c5c8a
buharilife <buharilife01@gmail.com>
2025-04-09 15:01:52 +0100

    i add a folder for file and validate the incoming file

---
+++ library/file_auth.php
@@ -0,0 +1,55 @@
+<!-- fileupload -->
+<?php
+ 
+
+
+    if (isset($_POST['submit'])) {
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode('.', $fileName);
+
+    $fileActualExt = strtolower(end($fileExt));
+    $allowed = array('jpg', 'png', 'jpeg', 'pdf');
+
+    if (in_array($fileActualExt, $allowed)) {
+
+        if ($fileError == 0) {
+            if ($fileSize < 1000000) {
+
+                $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+
+                $fileDestination = 'uploads/' . $fileNameNew;
+
+                move_uploaded_file($fileTmpName, $fileDestination);
+                // add the file with a modal a+
+                include "storephoto.php";
+                store($fileDestination);
+                echo 'upload successful upload';
+
+            } else {
+                echo " your file is too big";
+            }
+        } else {
+            echo "there was an error uploading this file ";
+        }
+    } else {
+        echo 'you cannot upload files of this type !';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ library/storephoto.php
@@ -0,0 +1,12 @@
+<?php
+function store($fileDestination)
+{
+    $fileName = "image.txt";
+    $file = fopen($fileName, "a+");
+    fwrite($file, $fileDestination . "\n");
+
+
+    return true;
+}
+
+?>
