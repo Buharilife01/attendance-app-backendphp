5760d2d346218775d1f29d77d317ac3689fdc825
unknown <ichukwuebuka26@gmail.com>
2025-04-09 13:34:06 +0100

    Signup controller created (initial)

--- index.php
+++ index.php
@@ -3,3 +3,23 @@ require_once(__DIR__."/path/paths.php");
 
 header("Access-contol-Allow-origin: *");
 header("accepts: Applications/JSON");
+
+try{
+    $userDetails = [
+        'last-name' => 'innocent',
+        'first-name' => 'ebuka',
+        'department' => 'polymer',
+        'email' => 'ichukwuea26@gmail.com',
+        'level' => 100,
+        'matric-no' => '23ddkkd',
+        'password' => '12222',
+        'confirm-password' => '12222'
+    ];
+    $test = new SignUpController();
+    
+    echo $test->signup($userDetails);
+
+} catch(Exception $e){
+    $error = $e->getMessage();
+    echo $error;
+}
--- path/paths.php
+++ path/paths.php
@@ -1,3 +1,5 @@
 <?php
 include(__DIR__."/../config/dbConfig.php");
 include(__DIR__."/../connection/DbConnection.php");
+
+include(__DIR__."/../controller/signUpController.php");
